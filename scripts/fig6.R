c0f05345c20d058ac342c444ca453aa97e8f5bbb
Martin Wilson <martin@pipegrep.co.uk>
2019-12-03 10:00:20 +0000

    bug fix

--- scripts/fig6.R
+++ scripts/fig6.R
@@ -43,6 +43,9 @@ metab_basis_list <- list(ala, asp, cr, gaba, glc, gln, glu, gpc, gsh, ins, lac,
 
 full_basis_list  <- append(metab_basis_list, list(mm_exp))
 
+full_basis <- sim_basis(full_basis_list, pul_seq = seq_slaser_ideal,
+                        xlim = c(0.5, 4.2))
+
 # metab values from de Graff book
 
 amps <- c( 0.80,  # 1  Ala
