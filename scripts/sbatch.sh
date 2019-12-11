c764c151c94b044e2a95b9a5ef33de8f54154d13
Martin Wilson <martin@pipegrep.co.uk>
2019-12-11 13:46:28 +0000

    sbatch

--- scripts/sbatch.sh
+++ scripts/sbatch.sh
@@ -14,7 +14,8 @@
 module purge
 module load bluebear
 module load bear-apps/2019a
-module load spant/0.18.0-foss-2019a-R-3.6.0
+# module load spant/0.18.0-foss-2019a-R-3.6.0
+module load spant/0.19.0-foss-2019a-R-3.6.0
 module load gridGraphics/0.4-1-foss-2019a-R-3.6.0
 
 Rscript fig4.R
