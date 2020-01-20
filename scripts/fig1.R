35ab633bc0e0f5249de2f9adfd422d29358ef697
Martin Wilson <martin@pipegrep.co.uk>
2020-01-20 16:47:46 +0000

    fig 1 looking better now

--- scripts/fig1.R
+++ scripts/fig1.R
@@ -8,7 +8,7 @@ if (Sys.getenv("RSTUDIO") == "1") {
   setwd(this.dir)
 }
 
-set.seed(1)
+set.seed(10)
 
 calc_yhat_sc_basis <- function(y, spline_basis, deriv_mat, lambda) {
   inv_mat   <- MASS::ginv(rbind(spline_basis, lambda ^ 0.5 * deriv_mat))
@@ -36,7 +36,7 @@ y_est <- res$yhat
 
 
 p1 <- function() {
-  par(bty = "L", mar = c(3.5,2,1.5,0.5), mgp = c(2.0, 0.7, 0))
+  par(bty = "L", mar = c(3.5, 2, 1.5, 0.5), mgp = c(2.0, 0.7, 0), cex = 0.7)
   offset <- matrix(rep(0:14, 200), nrow(spline_basis), ncol(spline_basis),
                    byrow = TRUE)
   
@@ -45,22 +45,22 @@ p1 <- function() {
 }
 
 p2 <- function() {
-  par(bty = "L", mar = c(3.5,4,1.5,4), mgp = c(2.0, 0.7, 0))
+  par(bty = "L", mar = c(3.5, 4, 1.5, 0.5), mgp = c(2.0, 0.7, 0), cex = 0.7)
   matplot(x, res$sc_basis, type = "l", lty = 1, col = c("blue"),
           ylim = c(0, 2.4), ylab = "y")
   
   lines(x, y)
   lines(x, y_est, col = "red")
-  legend(2.8, 3.0, c("data", "spline\nfit", "spline\nbasis"),
+  legend(2.1, 2.6, c("data", "spline fit", "spline basis"),
          col = c("black", "red", "blue"), lty = c(1,1,1),
          xpd = TRUE, box.lty = 0, y.intersp = 1.2)
 }
 
 full_plot <- plot_grid(p1, p2, labels = c('A', 'B'), label_size = 12,
-                       rel_widths = c(0.8,1))
+                       rel_widths = c(0.92, 1))
 
 print(full_plot)
 
-cairo_pdf("../figures/fig1.pdf", width = 6.92, height = 3.2, pointsize = 10)
+cairo_pdf("../figures/fig1.pdf", width = 6.92, height = 3.4, pointsize = 10)
 print(full_plot)
 dev.off()
