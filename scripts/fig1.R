ffc2e69197f533aedc73a4fe7d65e2b193e763d8
Martin Wilson <martin@pipegrep.co.uk>
2020-02-10 14:16:22 +0000

    remove print statement

--- scripts/fig1.R
+++ scripts/fig1.R
@@ -4,7 +4,6 @@ source("~/abfit/abfit.R")
 # change the working directory to the source file location
 # when "Sourcing" from the RStudio GUI
 if (Sys.getenv("RSTUDIO") == "1" & !is.null(parent.frame(2)$ofile)) {
-  print(parent.frame(2)$ofile)
   this.dir <- dirname(parent.frame(2)$ofile)
   setwd(this.dir)
 }
